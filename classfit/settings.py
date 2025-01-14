98918e47c282d8732d8a30d3139eaeb92dace8be
Jude Woolls <jwoolls01@gmail.com>
2025-01-14 13:39:49 +0000

    hide new secret key

--- classfit/settings.py
+++ classfit/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)d^d5)frc&7gbw4&h3!1@=l-!roan@qxgtg9e(tj(eoh)2^c5x'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
