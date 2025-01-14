ee0f09c2a2fc7d835835cced816ce6a1f03242e3
Jude Woolls <jwoolls01@gmail.com>
2025-01-14 16:35:40 +0000

    add template to view the event details

--- booking/views.py
+++ booking/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Event
 from django.views import generic
@@ -8,3 +8,13 @@ class EventList(generic.ListView):
     model = Event
     queryset = Event.objects.filter(status=0)
     template_name = 'booking/index.html'
+
+def event_detail(request, id):
+    queryset = Event.objects.filter(status=0)
+    event = get_object_or_404(queryset, id=id)
+
+    return render(
+        request,
+        "booking/event_detail.html",
+        {"event": event}
+    )
