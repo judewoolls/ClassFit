41f6213407b9111f0276ea6f6ae0ea4eed4125ba
Jude Woolls <jwoolls01@gmail.com>
2025-01-16 15:38:36 +0000

    move text to center and remove booking_homw.html

--- booking/urls.py
+++ booking/urls.py
@@ -2,7 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.EventList.as_view(), name='booking'),
     path('<str:date>/', views.event_search, name='event_search'),
     path('<str:date>/<int:id>/', views.event_detail, name='event_detail'),
 ]
--- classfit/settings.py
+++ classfit/settings.py
@@ -51,7 +51,7 @@ INSTALLED_APPS = [
 ]
 
 SITE_ID = 1
-LOGIN_REDIRECT_URL = '/booking'
+LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/accounts/login/'
 LOGIN_URL = 'account_login'
 
