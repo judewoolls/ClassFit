aecbbc34c15a758cc229ad0dcf5efd66a52f0442
Jude Woolls <jwoolls01@gmail.com>
2025-01-20 15:51:40 +0000

    add upcoming bookings to the homepage

--- homepage/views.py
+++ homepage/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from booking.models import Booking
+from django.contrib.auth.models import User
 
-## home page views
+# Home page view
 def home(request):
-    return render(request, 'homepage/home.html')
+    # Get the current user
+    user = request.user
+
+    # Query bookings made by the user and order by the date of the event
+    bookings = Booking.objects.filter(user=user).order_by('event__date_of_event')
+
+    return render(request, 'homepage/home.html', {
+        'bookings': bookings
+    })
