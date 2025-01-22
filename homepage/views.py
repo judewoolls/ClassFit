30425d186f25b66caaca1e589f94642b41f138d6
Jude Woolls <jwoolls01@gmail.com>
2025-01-22 13:36:04 +0000

    add recent scores to the homepage

--- homepage/views.py
+++ homepage/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from booking.models import Booking
+from logbook.models import Score
 from django.contrib.auth.models import User
 
 # Home page view
@@ -9,7 +10,10 @@ def home(request):
 
     # Query bookings made by the user and order by the date of the event
     bookings = Booking.objects.filter(user=user).order_by('event__date_of_event')
+    #Query scores made by the user
+    scores =  Score.objects.filter(user=user).order_by('-created_on')
 
     return render(request, 'homepage/home.html', {
-        'bookings': bookings
+        'bookings': bookings,
+        'scores': scores
     })
